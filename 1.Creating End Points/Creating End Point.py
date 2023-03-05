8fdd0869f8743cad72171314732b14c270baa06a
ictorv <victor.mygit@gmail.com>
2023-03-05 22:43:22 +0530

    Flask End Point

---
+++ 1.Creating End Points/Creating End Point.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    name="Victor"
+    return render_template("about.html",name=name)
+
+@app.route("/support")
+def support():
+    return "Thank you so much for visiting here"
+app.run(debug=True)
