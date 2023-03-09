313363807bb30cdade306fe638d6de5d200ca67d
ictorv <victor.mygit@gmail.com>
2023-03-09 06:02:08 +0530

    Error Message & Mail

--- 4.POST Request Login Form/app.py
+++ 4.POST Request Login Form/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template,request
+import smtplib
+
 app=Flask(__name__)
 @app.route('/')
 @app.route('/subscribe')
@@ -15,6 +17,20 @@ def form():
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     email = request.form.get("email")
+
+    message="You have been subscribet to my email news letter..."
+    server=smtplib.SMTP("smtp.gmail.com",587)
+    server.starttls()
+    server.login("victor.myid@gmail.com", "PASSWORD")
+    server.sendmail("victor.myid@gmail.com",email,message)
+
+    if not first_name or not last_name or not email:
+        error_statement="All Details are Necessary..."
+        return render_template("subscribe.html", error_statement=error_statement, 
+                               first_name=first_name, 
+                               last_name=last_name, 
+                               email=email)
+
     return render_template("form.html", first_name=first_name, last_name=last_name, email=email)
 
 app.run(debug=True)
